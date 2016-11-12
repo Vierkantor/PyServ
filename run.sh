6a9b5773a5e20a234d0d3481689168ca9997cf81
Vierkantor <vierkantor@vierkantor.com>
2016-11-12 16:22:42 +0000

    Also handle the case when no venv exists when running

--- run.sh
+++ run.sh
@@ -3,6 +3,13 @@
 # Execute the run script as the correct user
 if [ $USER != "py" ]; then
 	sudo -u py $0
+	exit $?
 fi
-source venv/bin/activate
+# Activate the virtual env if it exists
+if [ -d venv ]; then
+	source venv/bin/activate
+else
+	echo "Warning: you should install a virtual env in the venv directory"
+	echo "Hopefully, you're running in a VM with nothing else..."
+fi
 python3 run.py
