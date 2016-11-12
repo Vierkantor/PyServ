d9feacaf774f72a99d7843d52a4ca849cd42f273
Vierkantor <vierkantor@vierkantor.com>
2016-11-12 16:21:47 +0000

    Show a better error message when config is missing

--- pyserv/databrowse.py
+++ pyserv/databrowse.py
@@ -1,7 +1,11 @@
 from sys import exit
 
-# TODO: generate configuration automatically if it's missing
-from . import config
+try:
+	from . import config
+except ImportError:
+	print("""Cannot start: no configuration file found!
+	See pyserv/config.py.example for an example file.""")
+	exit(1)
 from . import app
 from .database import prepare_tables
 from . import view # load all the views
